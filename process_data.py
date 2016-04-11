e36dd57ed8a08c19488bfacd3fffc67356424242
Gregrs <gregrs@fastmail.co.uk>
2016-04-11 16:47:15 +0100

    Rename districts script

--- process_data.py
+++ process_data.py
@@ -3,6 +3,7 @@ from fhrs_osm import *
 db = Database()
 db.connect()
 
+# compute districts
 print "Adding district ID for FHRS establishments"
 db.add_fhrs_districts()
 print "Adding district ID for OSM entities"
