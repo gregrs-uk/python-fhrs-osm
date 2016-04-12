a2877a0f25ace344e4362f6ae10b95dfada8fb60
Gregrs <gregrs@fastmail.co.uk>
2016-04-12 13:12:49 +0100

    Improve structure of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-if [ -f "./shapefiles/district_borough_unitary_region.shp" ]
+if [ ! -f "./shapefiles/district_borough_unitary_region.shp" ]
 then
-	pip install psycopg2 && \
-	dropdb fhrs
-    createdb fhrs && \
-    psql -d fhrs -c "create extension postgis; create extension fuzzystrmatch;" && \
-    ./import_bline_districts.sh && \
-    python get_fhrs_data.py && \
-		python get_osm_data.py && \
-    python process_data.py && \
-	if [ ! -d "./html" ]
-	then
-		mkdir ./html
-	fi
-	if [ ! -d "./html/json" ]
-	then
-		mkdir ./html/json
-	fi
-    python create_output_files.py
-else
 	echo "Can't find the OS Boundary Line shapefiles. Please see"
 	echo "shapefiles/README-shapefiles.md for instructions."
+    exit 1
 fi
+
+pip install psycopg2 || exit 1
+dropdb --if-exists fhrs || exit 1
+createdb fhrs || exit 1
+psql -d fhrs -c "create extension postgis; create extension fuzzystrmatch;" || exit 1
+./import_bline_districts.sh || exit 1
+python get_fhrs_data.py || exit 1
+python get_osm_data.py || exit 1
+python process_data.py || exit 1
+if [ -d "./html" ]
+then
+	rm -rf ./html || exit 1
+fi
+mkdir ./html || exit 1
+mkdir ./html/json || exit 1
+python create_output_files.py || exit 1
