dc6bdeeecf9527f6200e1d5490d7309623f0979b
Gregrs <gregrs@fastmail.co.uk>
2016-04-12 09:43:09 +0100

    Setup.sh auto-creates JSON output directory

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ then
     ./import_bline_districts.sh && \
     python get_data.py && \
     python process_data.py && \
+		if [ ! -d "./html/json" ]
+		then
+			mkdir ./html/json
+		fi
     python create_geojsons.py
 else
 	echo "Can't find the OS Boundary Line shapefiles. Please see"
