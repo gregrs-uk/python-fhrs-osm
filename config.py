7232675fc9fbfda60c0c6ed1b9109ac69e4133f4
Gregrs <gregrs@fastmail.co.uk>
2016-08-30 10:44:30 +0000

    Config options for dev server instance

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config file to be read by both Python and BASH
 
 # postgresql database name
-dbname="fhrs"
+dbname="gregrs_fhrs"
 
 # fhrs download mode
 # small_test = Rugby and Warwick, west_mids = West Midlands, full = All regions
@@ -9,10 +9,10 @@ get_fhrs_mode="small_test"
 
 # do we want to use a filtered planet extract file (data/filtered.osm)
 # instead of querying using Overpass API?
-use_xml_file=False
+use_xml_file=True
 
 # path of pbf file if we're using a planet extract
 input_pbf="data/great-britain-latest.osm.pbf"
 
 # path of osmosis binary if we're using a planet extract
-osmosis_bin="~/Downloads/osmosis-latest/bin/osmosis"
+osmosis_bin="~/osmosis/bin/osmosis"
