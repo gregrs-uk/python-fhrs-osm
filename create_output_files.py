aac5ad88577f4d3e8cf822e26446f0214ee9e634
Gregrs <gregrs@fastmail.co.uk>
2016-09-01 10:07:14 +0100

    Remove Google Analytics code from master

--- create_output_files.py
+++ create_output_files.py
@@ -47,16 +47,6 @@ for dist in districts:
             border-collapse: collapse;
         }
     </style>
-
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-15864457-4', 'auto');
-      ga('send', 'pageview');
-    </script>
 </head>
 <body>
 
@@ -312,16 +302,6 @@ html = ("""
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css" />
-
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-15864457-4', 'auto');
-      ga('send', 'pageview');
-    </script>
 </head>
 
 <body>
