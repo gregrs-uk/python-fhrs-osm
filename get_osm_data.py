15b71cd3100de208303cb58cf765c657cb6d0e3f
Gregrs <gregrs@fastmail.co.uk>
2016-11-11 20:41:55 +0000

    Don't calculate extent of FHRS data if using OSM XML file
    Fix #30

--- get_osm_data.py
+++ get_osm_data.py
@@ -5,20 +5,20 @@ db = Database(dbname=config.dbname)
 con = db.connect()
 
 fhrs = FHRSDataset()
-
-print 'Calculating geographical extent of FHRS data, ignoring outliers'
-fhrs_bbox = fhrs.get_corrected_bbox(connection=con)
-
-# get OSM data within matching bounding box
 osm = OSMDataset()
+
 print "Creating OSM database table"
 osm.create_table(connection=con)
+
 if config.use_xml_file is True:
     print "Parsing OSM XML file"
     result = osm.parse_xml_file('data/filtered.osm')
     print "Writing OSM data to database"
     osm.write_result_nodes_and_ways(result=result, connection=con, filter_ways=False)
 else:
+    # get OSM data within matching bounding box
+    print 'Calculating geographical extent of FHRS data, ignoring outliers'
+    fhrs_bbox = fhrs.get_corrected_bbox(connection=con)
     print "Running Overpass query"
     result = osm.run_overpass_query(bbox=fhrs_bbox)
     if len(result.get_node_ids()) + len(result.get_way_ids()) < 1:
